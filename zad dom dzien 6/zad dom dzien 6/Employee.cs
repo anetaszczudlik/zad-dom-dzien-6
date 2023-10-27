30c6db5232bac56712425411e251fb4c5df0555b
Aneta53k <aneta.szczudlik@gmail.com>
2023-10-27 21:52:28 +0200

    Zadanie domowe dzien 6

---
+++ zad dom dzien 6/zad dom dzien 6/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace zad_dom_dzien_6
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
---
+++ zad dom dzien 6/zad dom dzien 6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+using zad_dom_dzien_6;
+
+Employee employee1 = new Employee("Jadwiga", "Spalina");
+Employee employee2 = new Employee("Janusz", "Tracz");
+Employee employee3 = new Employee("Kazimiera", "Solejuk");
+
+employee1.AddScore(2);
+employee1.AddScore(3);
+employee1.AddScore(6);
+employee1.AddScore(2);
+employee1.AddScore(1);
+
+employee2.AddScore(8);
+employee2.AddScore(6);
+employee2.AddScore(10);
+employee2.AddScore(7);
+employee2.AddScore(8);
+
+employee3.AddScore(4);
+employee3.AddScore(7);
+employee3.AddScore(1);
+employee3.AddScore(10);
+employee3.AddScore(3);
+
+
+List<Employee> employees = new List<Employee>()
+{ 
+    employee1, employee2, employee3 
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
+    }
+}
+
+Console.WriteLine("Pracownik z maksymalną liczbą punktów to " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + "," + " " + maxResult + "pkt.");
